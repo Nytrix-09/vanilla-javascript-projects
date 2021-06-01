491055cfbc7246b300bf365bc502e65d911c4216
Nytrix-09 <75318042+Nytrix-09@users.noreply.github.com>
2021-06-01 17:59:06 +0530

    Update script.js

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,1 +1,78 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos){
+    todos.forEach(todo => {
+        addToDo(todo)
+    });
+}
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault();
+
+    addToDo();
+});
+
+
+function addToDo(todo)
+{   let todoText=input.value;
+    if(todo)
+    {
+        todoText=todo.text;
+    }
+    
+
+    if(todoText)
+    {
+        const todoEle = document.createElement("li");
+        if(todo && todo.completed)
+        {todoEle.classList.add('completed');}
+        todoEle.innerText=todoText;
+        
+
+
+        todoEle.addEventListener('click', () => {
+            todoEle.classList.toggle("completed");
+
+            updateLs();
+        });
+        
+        todoEle.addEventListener('contextmenu',(e)=>{
+            e.preventDefault();
+
+            todoEle.remove();
+
+            updateLs();
+        });
+        
+        todosUL.appendChild(todoEle);
+
+        input.value="";
+
+        updateLs();
+    }
+
+}
+
+
+function updateLs()
+{
+    const toDoEle = document.querySelectorAll('li');
+
+    const toDos = [];
+
+    toDoEle.forEach(toDoEle => {
+        toDos.push({
+            text: toDoEle.innerText,
+            completed: toDoEle.classList.contains
+            ('completed')
+        });
+    });
+
+    localStorage.setItem("todos" , JSON.stringify(toDos));
+}
+
 
